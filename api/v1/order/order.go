ed23583b6bafa131cffd19aa9955535e2f9f74ea
ucdo <phpsarc@gmail.com>
2025-04-30 17:34:57 +0800

    Update order.go

--- api/v1/order/order.go
+++ api/v1/order/order.go
@@ -22,7 +22,7 @@ func Pay(c *gin.Context) {
 		response.Error(c, err)
 		return
 	}
-	order.OrderPay(c, param)
+	order.Pay(c, param)
 }
 
 func Pay2(c *gin.Context) {
@@ -31,20 +31,10 @@ func Pay2(c *gin.Context) {
 		response.Error(c, err)
 		return
 	}
-	order.OrderPay(c, param)
+	order.Pay(c, param)
 }
 
-// TODO 这里要优化成消息队列，因为如果通过时间段去查询，会造成表锁或者范围锁，因为没有对时间加索引，
-
-func Expire(c *gin.Context) {
-	// 通过传入的订单id以及订单唯一编号来确定订单是否过期。额这玩意儿要么让队列来整吧，定时就行了
-	param := &dto.OrderExpire{}
-	if err := c.ShouldBind(param); err != nil {
-		response.Error(c, err)
-		return
-	}
-	order.OrderExpire(c, param)
-}
+// TODO expire 这里要优化成消息队列，因为如果通过时间段去查询，会造成表锁或者范围锁，因为没有对时间加索引，
 
 // CancelOrder 取消订单
 func CancelOrder(c *gin.Context) {
